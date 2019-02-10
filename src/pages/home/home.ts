64ceb947d5cb193d9f45f5d1536aa1759fa13829
Andre Justyn <bernadus.nyoman@student.umn.ac.id>
2019-02-10 23:10:26 +0700

    First commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { FirstPage } from '../first/first';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  button1() {
+    this.navCtrl.push(FirstPage);
+  }
+
+}
